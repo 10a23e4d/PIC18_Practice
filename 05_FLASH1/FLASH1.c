6d30fbfc05e66e2d0bdc8995efff4675d057d00f
B25A5006 <s25A5006PM@chibatech.ac.jp>
2026-02-17 17:03:28 +0900

    FLASH用コード仮

--- 05_FLASH1/FLASH1.c
+++ 05_FLASH1/FLASH1.c
@@ -1,97 +1,160 @@
-/////LED1/////
-//#include <18F67J94.h>
-#include "LED1.h"
+#include "FLASH1.h"
 
+//=============================================================================
+//  Pin & Communication Settings
+//=============================================================================
 
+// --- UART1 Settings (PPS) ---
 #pin_select U1TX = PIN_F7
 #pin_select U1RX = PIN_F5
-#use rs232(baud=9600, UART1, parity=N,bits=8,stream=PORT1)
+#use rs232(baud=9600, UART1, parity=N, bits=8, stream=PORT1)
 
-#pin_select U2TX = PIN_D3
-#pin_select U2RX =    PIN_D2
-#use rs232(baud=9600, UART2, parity=N,bits=8,stream=PORT2)
+// --- SPI1 Settings (PPS & Port A) ---
+// 【重要】PIC18F67J94はPPS設定が必須です。これがないと信号が出ません。
+#pin_select SDI1    = PIN_A0  // MISO: Flash DO -> PIC DI
+#pin_select SDO1    = PIN_A1  // MOSI: PIC DO -> Flash DI
+#pin_select SCK1OUT = PIN_A3  // CLK : Clock
 
-#pin_select U3TX = PIN_E5
-#pin_select U3RX = PIN_E4
-#use rs232(baud=9600, UART3, parity=N,bits=8,stream=PORT3)
+// 基板の配線定義
+#define SPI1_CS  PIN_A2
 
-#pin_select U4TX = PIN_E3
-#pin_select U4RX = PIN_F2
-#use rs232(baud=9600, UART4, parity=N,bits=8,stream=PORT4)
-
-#define SPI1_CS PIN_A2
-#use spi (MASTER, CLK = PIN_A3, DI = PIN_A0, DO = PIN_A1,BAUD=1000000, MODE=0, BITS=8, STREAM=SPI1)
-
-#define SPI2_CS PIN_B3
-#use spi (MASTER, CLK = PIN_B2, DI = PIN_B5, DO = PIN_B4,BAUD=1000000, MODE=0, BITS=8, STREAM=SPI1)
-
-#define SPI3_CS PIN_E2
-#use spi (MASTER, CLK = PIN_E1, DI = PIN_E0, DO = PIN_E6,BAUD=1000000, MODE=0, BITS=8, STREAM=SPI1)
+// SPI設定
+#use spi(MASTER, CLK=PIN_A3, DI=PIN_A0, DO=PIN_A1, BAUD=1000000, MODE=0, BITS=8, STREAM=SPI1)
 
 
-#define DIO_3 PIN_G0
-#define DIO_4 PIN_G1
-#define DIO_5 PIN_G2
-#define DIO_6 PIN_G3
-#define DIO_8 PIN_G4
-#define DIO_12 PIN_F6
-#define DIO_27 PIN_A5
-#define DIO_28 PIN_A4
-#define DIO_29 PIN_C1
-#define DIO_30 PIN_C0
-#define DIO_31 PIN_C6
-#define DIO_32 PIN_C7
-#define DIO_33 PIN_C2
-#define DIO_34 PIN_C3
-#define DIO_35 PIN_C4
-#define DIO_36 PIN_C5
-#define DIO_47 PIN_B1
-#define DIO_48 PIN_B0
-#define DIO_49 PIN_D7
-#define DIO_50 PIN_D6
-#define DIO_51 PIN_D5
-#define DIO_52 PIN_D4
-#define DIO_55 PIN_D1
-#define DIO_58 PIN_D0
-#define DIO_59 PIN_E7
+//=============================================================================
+//  Flash Memory Commands (MT25QL128ABA)
+//=============================================================================
+#define FLASH_CMD_WREN      0x06
+#define FLASH_CMD_RDID      0x9F
+#define FLASH_CMD_RDSR      0x05
+#define FLASH_CMD_READ      0x03
+#define FLASH_CMD_PP        0x02
+#define FLASH_CMD_SE        0x20
 
+//=============================================================================
+//  Low Level Functions
+//=============================================================================
 
-void main()
-{
-    port_b_pullups(TRUE);
-
-    int1 current_state = 1;
-    int1 last_state = 1;
-    int8 i;
-
-
-    fprintf(PORT1, "Starting LED2 Program\r\n");
-
-    while (TRUE)
-    {
-        current_state = input(DIO_47);
-
-        if (current_state == 0 && last_state == 1)
-        {
-            delay_ms(20);
-            if(input(DIO_47) == 0)
-            {
-                fprintf(PORT1, "Button Pressed\r\n");
-                fprintf(PORT1, "Starting Blinking LED\r\n");
-                for (i = 0; i < 5; i++)
-                {
-                    output_high(DIO_3);
-                    fprintf(PORT1, "LED ON (%d/5)\r\n", i + 1);
-                    delay_ms(500);
-                    output_low(DIO_3);
-                    fprintf(PORT1, "LED OFF (%d/5)\r\n", i + 1);
-                    delay_ms(500);
-                }
-                fprintf(PORT1, "Finished Blinking LED\r\n");
-            }
-        }
-        last_state = current_state;
-        delay_ms(10);
-    }
+void Flash_Write_Enable() {
+    output_low(SPI1_CS);
+    spi_xfer(SPI1, FLASH_CMD_WREN);
+    output_high(SPI1_CS);
 }
 
+void Flash_Wait_Busy() {
+    int8 status;
+    do {
+        output_low(SPI1_CS);
+        spi_xfer(SPI1, FLASH_CMD_RDSR);
+        status = spi_xfer(SPI1, 0x00);
+        output_high(SPI1_CS);
+    } while (status & 0x01);
+}
+
+void Flash_Read_ID(int8 *mfg_id, int16 *dev_id) {
+    int8 d1, d2;
+
+    output_low(SPI1_CS);
+    spi_xfer(SPI1, FLASH_CMD_RDID);
+
+    *mfg_id = spi_xfer(SPI1, 0x00);
+    d1 = spi_xfer(SPI1, 0x00);
+    d2 = spi_xfer(SPI1, 0x00);
+
+    output_high(SPI1_CS);
+
+    *dev_id = make16(d1, d2);
+}
+
+void Flash_Sector_Erase(int32 address) {
+    Flash_Wait_Busy();
+    Flash_Write_Enable();
+
+    output_low(SPI1_CS);
+    spi_xfer(SPI1, FLASH_CMD_SE);
+    spi_xfer(SPI1, (address >> 16) & 0xFF);
+    spi_xfer(SPI1, (address >> 8)  & 0xFF);
+    spi_xfer(SPI1,  address        & 0xFF);
+    output_high(SPI1_CS);
+
+    Flash_Wait_Busy();
+}
+
+void Flash_Write_Byte(int32 address, int8 data) {
+    Flash_Wait_Busy();
+    Flash_Write_Enable();
+
+    output_low(SPI1_CS);
+    spi_xfer(SPI1, FLASH_CMD_PP);
+    spi_xfer(SPI1, (address >> 16) & 0xFF);
+    spi_xfer(SPI1, (address >> 8)  & 0xFF);
+    spi_xfer(SPI1,  address        & 0xFF);
+    spi_xfer(SPI1, data);
+    output_high(SPI1_CS);
+
+    Flash_Wait_Busy();
+}
+
+void Flash_Read_Data(int32 address, int8 *buffer, int16 length) {
+    int16 i;
+    Flash_Wait_Busy();
+
+    output_low(SPI1_CS);
+    spi_xfer(SPI1, FLASH_CMD_READ);
+    spi_xfer(SPI1, (address >> 16) & 0xFF);
+    spi_xfer(SPI1, (address >> 8)  & 0xFF);
+    spi_xfer(SPI1,  address        & 0xFF);
+
+    for(i=0; i<length; i++) {
+        buffer[i] = spi_xfer(SPI1, 0x00);
+    }
+    output_high(SPI1_CS);
+}
+
+//=============================================================================
+//  Main Function
+//=============================================================================
+void main() {
+    int8 mfg_id;
+    int16 dev_id;
+    int32 target_addr = 0x001000;
+    int8 write_val = 0x55;
+    int8 read_val = 0;
+
+    // --- Init ---
+    setup_adc_ports(NO_ANALOGS); // アナログ入力無効化
+    output_drive(SPI1_CS);
+    output_high(SPI1_CS);
+
+    delay_ms(100);
+
+    fprintf(PORT1, "\r\n\r\n=== Flash Test (PPS Added) ===\r\n");
+
+    // 1. Read Chip ID
+    Flash_Read_ID(&mfg_id, &dev_id);
+    fprintf(PORT1, "ID Read: Mfg=0x%X, Dev=0x%LX\r\n", mfg_id, dev_id);
+
+    if(mfg_id == 0x20) {
+        fprintf(PORT1, "[OK] Micron Flash Detected.\r\n");
+    } else {
+        fprintf(PORT1, "[ERR] ID Read Failed. Check connections.\r\n");
+    }
+
+    // 2. Erase/Write/Read
+    Flash_Sector_Erase(target_addr);
+    Flash_Write_Byte(target_addr, write_val);
+    Flash_Read_Data(target_addr, &read_val, 1);
+
+    fprintf(PORT1, "W:0x%X, R:0x%X -> ", write_val, read_val);
+
+    if(write_val == read_val) {
+        fprintf(PORT1, "SUCCESS!\r\n");
+    } else {
+        fprintf(PORT1, "FAIL\r\n");
+    }
+
+    while(TRUE) {
+        delay_ms(1000);
+    }
+}
